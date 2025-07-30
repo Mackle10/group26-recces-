9156fb29c9e057da4b84a0836a63e3c889a3b15f
atwix17500 <prudcatwix@gmail.com>
2025-07-30 15:25:36 +0300

    Update build.gradle.kts

--- wastemanagement/android/build.gradle.kts
+++ wastemanagement/android/build.gradle.kts
@@ -6,7 +6,7 @@ allprojects {
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.value(newBuildDir) 
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
